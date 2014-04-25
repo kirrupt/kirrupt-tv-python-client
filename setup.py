e4dbf47b4aa6359f7184c97860ccdf7dfc7c44b3
Dejan Levec <dejan@dejanlevec.com>
2014-04-25 18:27:43 +0200

    added support for python 3.3

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 #!/bin/env python
 
+import sys
+
 from setuptools import setup
 
-VERSION = '0.2'
+
+VERSION = '0.3'
+
+
+install_requires = [
+    'requests',
+    'pytz'
+]
+
+if sys.version_info < (3, 0):
+    install_requires.append("email")
 
 setup(
     name='kirrupt-tv',
     version=VERSION,
     description='A Python client for the Kirrupt TV API.',
-    install_requires=[
-        'requests',
-        'pytz',
-        'email'
-    ],
+    install_requires=install_requires,
     author='Kirrupt',
     author_email='info@kirrupt.com',
     url='https://github.com/kirrupt/kirrupt-tv-python-client',
